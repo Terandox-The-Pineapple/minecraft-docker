60096473d9c835c0eb8a4f27daf387b691defb23
René-Gilbert Rückert <rene-gilbert99@web.de>
2023-09-29 15:22:09 +0200

    fixed formating in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+SERVER_PATH="/home/minecraft-server/server"
+
 if [[ -z $FORGE ]] ; then echo Forge-Mode disabled ; else {
     echo Forge-Mode enabled
     if test -f ".forge-done" ; then echo Forge already installed ; else {
@@ -6,26 +9,30 @@ if [[ -z $FORGE ]] ; then echo Forge-Mode disabled ; else {
         FORGE_JAR=forge-*-installer.jar
         java -jar ${FORGE_JAR} --installServer
         rm ${FORGE_JAR}
-        if test -f "user_jvm_args.txt" ; then echo "-Xmx$MAX_RAM" >> user_jvm_args.txt ; fi
+        if test -f "$SERVER_PATH/user_jvm_args.txt" ; then echo "-Xmx$MAX_RAM" >> user_jvm_args.txt ; fi
         touch .forge-done
     } fi
 } fi
 
 echo Starting server
 
-
 if [[ -z $FORGE && -z $FABRIC ]] ; then {
-    if test -f "run.sh" ; then ./run.sh ; else {
+    echo Minecraft-Vanilla
+    if test -f "$SERVER_PATH/run.sh" ; then "$SERVER_PATH/run.sh" ; else {
         SERVER_FILE=*.jar
         java -Xmx$MAX_RAM -jar $SERVER_FILE --nogui
     } fi
-} else if [[ -z $FABRIC ]] ; then {
-    if test -f "run.sh" ; then ./run.sh ; else {
+} fi 
+
+if [[ -z $FABRIC ]] ; then {
+    echo Minecraft-Forge
+    if test -f "$SERVER_PATH/run.sh" ; then "$SERVER_PATH/run.sh" ; else {
         FORGE_SERVER=*forge*.jar
         java -Xmx$MAX_RAM -jar $FORGE_SERVER --nogui
     } fi
 } else {
-    if test -f "run.sh" ; then ./run.sh ; else {
+    echo Minecraft-Fabric
+    if test -f "$SERVER_PATH/run.sh" ; then "$SERVER_PATH/run.sh" ; else {
         FABRIC_SERVER=*fabric*.jar
         java -Xmx$MAX_RAM -jar $FABRIC_SERVER --nogui
     } fi
