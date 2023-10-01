a34733f3f3bf76df08950a9d4787e549bb7632bb
René-Gilbert Rückert <rene-gilbert99@web.de>
2023-10-01 19:33:12 +0200

    Added mcrcon to the image and got it working on default. Also added script to connect to server console

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,17 +4,23 @@ SERVER_PATH="/home/minecraft-server/server"
 
 if [[ -z $FORGE ]] ; then echo Forge-Mode disabled ; else {
     echo Forge-Mode enabled
-    if test -f ".forge-done" ; then echo Forge already installed ; else {
+    if test -f ".forge-done.lock" ; then echo Forge already installed ; else {
         echo Starting Forge installation
         FORGE_JAR=forge-*-installer.jar
         java -jar ${FORGE_JAR} --installServer
         rm ${FORGE_JAR}
         if test -f "$SERVER_PATH/user_jvm_args.txt" ; then echo -e "\n-Xmx$MAX_RAM" >> user_jvm_args.txt ; fi
-        touch .forge-done
+        touch .forge-done.lock
     } fi
 } fi
 
 if [[ -z $FABRIC ]] ; then echo Fabric-Mode disabled ; else echo Starting Fabric ; fi
+
+if ! test -f .rcon.lock ; then {
+    sed -i 's/enable-rcon=false/enable-rcon=true/' $SERVER_PATH/server.properties
+    sed -i 's/rcon.password=/rcon.password=Uwu666@13-Ara_Ara/' $SERVER_PATH/server.properties
+    touch .rcon.lock
+} fi
 
 echo Starting server
 
